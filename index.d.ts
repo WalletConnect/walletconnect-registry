8dd4b8d7ba7199e6459a9225162ea2e38a35944d
Pedro Gomes <pedrogomes94@gmail.com>
2021-05-15 14:58:53 +0200

    draft schema

--- index.d.ts
+++ index.d.ts
@@ -1,36 +1,53 @@
 declare module "@walletconnect/registry" {
-  export interface AppEntry {
-    id: string;
+  export interface AppPlatforms {
+    browser?: string;
+    ios?: string;
+    android?: string;
+    mac?: string;
+    windows?: string;
+    linux?: string;
+  }
+
+  export interface AppLinkingOptions {
+    native?: string;
+    universal?: string;
+  }
+
+  export interface AppLinking {
+    mobile?: AppLinkingOptions;
+    desktop?: AppLinkingOptions;
+  }
+
+  export interface AppMetadataColors {
+    primary?: string;
+    secondary?: string;
+  }
+
+  export interface AppMetadata {
+    homepage?: string;
+    shortName?: string;
+    customLogo?: string;
+    colors?: AppMetadataColors;
+  }
+
+  export type AppType = "non-signer" | "signer" | "hybrid";
+
+  export interface AppDescription {
     name: string;
+    type: AppType;
     homepage: string;
-    chains: string[];
-    app: {
-      browser: string;
-      ios: string;
-      android: string;
-      mac: string;
-      windows: string;
-      linux: string;
-    };
-    mobile: {
-      native: string;
-      universal: string;
-    };
-    desktop: {
-      native: string;
-      universal: string;
-    };
-    metadata: {
-      shortName: string;
-      colors: {
-        primary: string;
-        secondary: string;
-      };
-    };
+    categories: string[];
+    platforms: AppPlatforms;
+    metadata?: AppMetadata;
+  }
+  export interface AppProvider {
+    id: string;
+    host: string;
+    apps: AppDescription[];
   }
 
   export type AppRegistry = {
-    [id: string]: AppEntry;
+    [id: string]: App;
   };
 
   export default AppRegistry;
