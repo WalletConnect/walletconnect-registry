0034317ddb57425c50c8f125ebe89697adfaa7f1
Pedro Gomes <pedrogomes94@gmail.com>
2021-08-16 11:52:58 +0100

    Revert "update WallETH"
    This reverts commit b1b39785cb5843e1486b32bce271dcdaf3d86978.

--- template/index.js
+++ template/index.js
@@ -2,35 +2,35 @@ const path = require("path");
 
 module.exports = {
   // REQUIRED (location of the logo - png or jpg)
-  logo: path.join(__dirname, "fuse.png"),
+  logo: path.join(__dirname, "logo.jpg"),
   // REQUIRED
-  name: "fuse.cash",
+  name: "WalletConnect",
   // REQUIRED
-  homepage: "https://fuse.cash/",
+  homepage: "https://walletconnect.org",
   // REQUIRED
-  chains: ["eip155:122"],
+  chains: ["eip155:1"],
   // REQUIRED (at least one)
   app: {
-    browser: "",
-    ios: "https://apps.apple.com/us/app/fuse-cash/id1559937899",
-    android: "https://play.google.com/store/apps/details?id=io.fuse.cash",
+    browser: "https://example.walletconnect.org",
+    ios: "",
+    android: "",
     mac: "",
     windows: "",
     linux: "",
   },
   mobile: {
-    native: "fuse.cash:",
-    universal: "https://app.fuse.cash/",
+    native: "",
+    universal: "",
   },
   desktop: {
     native: "",
     universal: "",
   },
   metadata: {
-    shortName: "fuse.cash",
+    shortName: "",
     colors: {
-      primary: "#37D888",
-      secondary: "#37D888",
+      primary: "",
+      secondary: "",
     },
   },
 };
