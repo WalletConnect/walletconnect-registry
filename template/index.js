7675356d001b6b5eb7a9e8ddf5bee42c1517958c
Pedro Gomes <pedrogomes94@gmail.com>
2021-09-17 13:39:33 +0200

    add julwallet

--- template/index.js
+++ template/index.js
@@ -2,35 +2,47 @@ const path = require("path");
 
 module.exports = {
   // REQUIRED (location of the logo - png or jpg)
-  logo: path.join(__dirname, "logo.jpg"),
+  logo: path.join(__dirname, "julwallet.png"),
   // REQUIRED
-  name: "WalletConnect",
+  name: "JulWallet",
   // REQUIRED
-  homepage: "https://walletconnect.org",
+  homepage: "https://justliquidity.org/",
   // REQUIRED
-  chains: ["eip155:1"],
+  chains: [
+    "eip155:1",
+    "eip155:3",
+    "eip155:4",
+    "eip155:5",
+    "eip155:42",
+    "eip155:56",
+    "eip155:88",
+    "eip155:128",
+    "eip155:137",
+    "eip155:250",
+  ],
   // REQUIRED (at least one)
   app: {
-    browser: "https://example.walletconnect.org",
-    ios: "",
-    android: "",
+    browser:
+      "https://chrome.google.com/webstore/detail/julwallet/fgkldlaelopgncepgpncndebgdddikii?hl=en-US",
+    ios: "https://apps.apple.com/app/julwallet/id1555914591",
+    android: "https://play.google.com/store/apps/details?id=julwallet.com",
     mac: "",
     windows: "",
     linux: "",
   },
   mobile: {
-    native: "",
-    universal: "",
+    native: "julwallet:",
+    universal: "https://justliquidity.org/",
   },
   desktop: {
     native: "",
     universal: "",
   },
   metadata: {
-    shortName: "",
+    shortName: "JulWallet",
     colors: {
-      primary: "",
-      secondary: "",
+      primary: "#c84ce4",
+      secondary: "#3f00ff",
     },
   },
 };
