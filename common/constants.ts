5c053f6955c433e5ab83ecedbbc5899de939b281
Chad <chadyj@users.noreply.github.com>
2021-10-06 08:09:41 +0200

    update discord links

--- common/constants.ts
+++ common/constants.ts
@@ -1,4 +1,4 @@
 // internal and external links (urls)
 export const GITHUB_URL = "https://github.com/walletconnect";
-export const DISCORD_URL = "https://discord.gg/jhxMvxP";
+export const DISCORD_URL = "https://discord.gg/cB54BwPGru";
 export const TWITTER_URL = "https://twitter.com/walletconnect";
